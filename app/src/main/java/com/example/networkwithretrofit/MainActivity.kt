38ae800baa6c26c8bf505ecb3dd797ca88f8613f
manmohan <manmohan.singh@zopsmart.com>
2022-01-11 20:25:58 +0530

    first commit

---
+++ app/src/main/java/com/example/networkwithretrofit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.networkwithretrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
