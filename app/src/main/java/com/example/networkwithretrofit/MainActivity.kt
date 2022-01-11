51efc411a698fe331576c4717b5b5918435fbe9f
manmohan <manmohan.singh@zopsmart.com>
2022-01-11 21:46:36 +0530

    get data from server

--- app/src/main/java/com/example/networkwithretrofit/MainActivity.kt
+++ app/src/main/java/com/example/networkwithretrofit/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.networkwithretrofit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val retService: AlbumService =
+            RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val listOfAlbums = retService.getAlbums()
+            emit(listOfAlbums)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumsList: MutableListIterator<AlbumsItem>? = it.body()?.listIterator()
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem: AlbumsItem = albumsList.next()
+                    Log.i("MYTAG", albumsItem.title)
+                }
+            }
+        })
     }
 }
