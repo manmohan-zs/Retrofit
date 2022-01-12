ae31dffa20e149718a56f5772fb4b1030321a138
manmohan <manmohan.singh@zopsmart.com>
2022-01-12 09:20:55 +0530

    introduced logging interceptors

--- app/src/main/java/com/example/networkwithretrofit/RetrofitInstance.kt
+++ app/src/main/java/com/example/networkwithretrofit/RetrofitInstance.kt
@@ -1,18 +1,26 @@
 package com.example.networkwithretrofit
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
     companion object {
         val BASE_URL = "https://jsonplaceholder.typicode.com"
+        val interceptor: HttpLoggingInterceptor =
+            HttpLoggingInterceptor().apply { this.level = HttpLoggingInterceptor.Level.BODY }
+        val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+        }.build()
+
         fun getRetrofitInstance(): Retrofit {
             return Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(
                 GsonConverterFactory.create(
                     GsonBuilder().create()
                 )
-            ).build()
+            ).client(client).build()
         }
     }
 }
